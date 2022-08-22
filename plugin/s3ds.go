7394f2cb1f217b356c630d9efa6f96e8dff46b64
Sergey Ukustov <sergey@ukstv.me>
2022-08-22 18:39:49 +0300

    wip

--- plugin/s3ds.go
+++ plugin/s3ds.go
@@ -3,7 +3,7 @@ package plugin
 import (
 	"fmt"
 
-	s3ds "github.com/3box/go-ds-s3"
+	s3ds "github.com/ukstv/go-ds-s3"
 	"github.com/ipfs/go-ipfs/plugin"
 	"github.com/ipfs/go-ipfs/repo"
 	"github.com/ipfs/go-ipfs/repo/fsrepo"
